f3f6144afc2007e752ebd503a61afd5546bb4e7c
hushhw <hushhw@sina.cn>
2017-12-20 08:43:58 +0800

    add

--- 18MultiAttributeColor/MultiAttributeColor.js
+++ 18MultiAttributeColor/MultiAttributeColor.js
@@ -13,6 +13,7 @@ var VSHADER_SOURCE =
     '}\n';
 
 var FSHADER_SOURCE=
+    'precision mediump float;\n' +//!!! 需要声明浮点数精度，否则报错No precision specified for (float)  
     'varying vec4 v_Color;\n' +
     'void main(){\n'+
     'gl_FragColor = v_Color;\n'+
